eb009529cf37d948d6253a90b77e59bb148976cd
veprogames <75524847+veprogames@users.noreply.github.com>
2024-03-22 13:47:15 +0100

    add more control options

--- car/car.gd
+++ car/car.gd
@@ -7,13 +7,11 @@ const SPEED := 180_000.0
 @onready var wheel_r: RigidBody2D = $PinJoint2D2/WheelR
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("player_brake"):
 		wheel_l.apply_torque(-SPEED)
 		wheel_r.apply_torque(-SPEED)
-		#wheel_l.angular_velocity = -SPEED
-		#wheel_r.angular_velocity = -SPEED
 		apply_torque(SPEED)
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("player_gas"):
 		wheel_l.apply_torque(SPEED)
 		wheel_r.apply_torque(SPEED)
 		apply_torque(-SPEED)
