5485152a640690074687b434085b50f51a42e1f0
veprogames <75524847+veprogames@users.noreply.github.com>
2024-03-23 10:55:49 +0100

    wobbly head, death mechanic

--- car/car.gd
+++ car/car.gd
@@ -11,6 +11,10 @@ var touch_brake: bool = false
 
 @onready var viewport_rect: Rect2 = get_viewport_rect()
 
+@onready var timer_death: Timer = $TimerDeath
+
+@onready var pin_joint_2d_neck: PinJoint2D = $Head/PinJoint2DNeck
+
 func _input(event: InputEvent) -> void:
 	var touch_event: InputEventScreenTouch = event as InputEventScreenTouch
 	if touch_event != null:
@@ -23,11 +27,28 @@ func _input(event: InputEvent) -> void:
 			touch_brake = touch_event.pressed
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("player_brake") or touch_brake:
-		wheel_l.apply_torque(-SPEED)
-		wheel_r.apply_torque(-SPEED)
-		apply_torque(SPEED)
-	elif Input.is_action_pressed("player_gas") or touch_gas:
-		wheel_l.apply_torque(SPEED)
-		wheel_r.apply_torque(SPEED)
-		apply_torque(-SPEED)
+	if not is_dead():
+		if Input.is_action_pressed("player_brake") or touch_brake:
+			wheel_l.apply_torque(-SPEED)
+			wheel_r.apply_torque(-SPEED)
+			apply_torque(SPEED)
+		elif Input.is_action_pressed("player_gas") or touch_gas:
+			wheel_l.apply_torque(SPEED)
+			wheel_r.apply_torque(SPEED)
+			apply_torque(-SPEED)
+
+func break_neck() -> void:
+	pin_joint_2d_neck.node_a = ""
+	pin_joint_2d_neck.node_b = ""
+
+func is_dead() -> bool:
+	return !timer_death.is_stopped()
+
+func _on_head_body_entered(body: Node) -> void:
+	if body is SimpleTerrain:
+		if timer_death.is_stopped():
+			timer_death.start()
+			
+			break_neck()
+			await timer_death.timeout
+			get_tree().reload_current_scene.call_deferred()
