d82ae827f6e2edd705a0b3b8c0c95940f0042a3b
veprogames <75524847+veprogames@users.noreply.github.com>
2024-03-27 21:01:04 +0100

    add desert stage

--- level/terrain/generation/values/base_value_generator.gd
+++ level/terrain/generation/values/base_value_generator.gd
@@ -1,5 +1,8 @@
 class_name BaseValueGenerator
 extends Resource
 
+func _ready() -> void:
+	pass
+
 func get_value(_meters: float) -> float:
 	return 0
---
+++ level/terrain/generation/values/sine_value_generator.gd
@@ -0,0 +1,15 @@
+class_name SineValueGenerator
+extends BaseValueGenerator
+
+@export var period: float
+@export var min_value: float = -1.0
+@export var max_value: float = 1.0
+
+func _ready() -> void:
+	super._ready()
+	assert(period > 0.0)
+
+func get_value(meters: float) -> float:
+	var x: float = TAU * meters / period
+	var sine = sin(x)
+	return clampf(sine, min_value, max_value)
