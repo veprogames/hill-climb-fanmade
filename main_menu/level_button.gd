976b907f0ff85ae50faf47b4e3c9a2cadbb2b448
veprogames <75524847+veprogames@users.noreply.github.com>
2024-03-31 22:23:02 +0200

    show highscores on level button

--- main_menu/level_button.gd
+++ main_menu/level_button.gd
@@ -1,16 +1,19 @@
 class_name LevelButton
-extends TextureRect
+extends TextureButton
 
 @export var level_data: LevelData
 
 @onready var label_title: Label = $LabelTitle
+@onready var label_highscore: Label = $HBoxContainerHighscore/LabelHighscore
 
 func _ready() -> void:
 	assert(level_data != null)
 	
-	texture = level_data.thumbnail
+	var highscore: float = Game.save.highscores.get_highscore(level_data)
+	
+	texture_normal = level_data.thumbnail
 	label_title.text = level_data.title
+	label_highscore.text = "%s m" % F.F(highscore)
 
-
-func _on_button_play_pressed() -> void:
+func _on_pressed() -> void:
 	get_tree().change_scene_to_file(level_data.scene_path)
