9d8663d3ba990fa7d4b53a82eceb24ee8adfe6f7
veprogames <75524847+veprogames@users.noreply.github.com>
2024-03-31 15:23:28 +0200

    add level data data structure

---
+++ level/data/level_data.gd
@@ -0,0 +1,8 @@
+class_name LevelData
+extends Resource
+
+@export var id: String
+@export var title: String
+@export_multiline var description: String
+@export var thumbnail: Texture
+@export_file("*.tscn") var scene_path: String
--- main_menu/level_button.gd
+++ main_menu/level_button.gd
@@ -1,16 +1,16 @@
 class_name LevelButton
 extends TextureRect
 
-@export_file("*.tscn") var scene_path: String
-@export var title: String
-@export var thumbnail: Texture
+@export var level_data: LevelData
 
 @onready var label_title: Label = $LabelTitle
 
 func _ready() -> void:
-	texture = thumbnail
-	label_title.text = title
+	assert(level_data != null)
+	
+	texture = level_data.thumbnail
+	label_title.text = level_data.title
 
 
 func _on_button_play_pressed() -> void:
-	get_tree().change_scene_to_file(scene_path)
+	get_tree().change_scene_to_file(level_data.scene_path)
